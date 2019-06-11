176b9b8fe10800b53b10402861eb1ea236d78aa9
joaodematejr <joaodematejr@gmail.com>
2019-06-10 23:19:44 -0300

    att

---
+++ arkanoid/Arkanoid/Assets/scr.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+   void OnCollisionEnter2D(Collision2D collision){
+
+   		GameObject collider = collision.collider.gameObject;
+   		GameObject.Destroy (collider);
+   		
+   }
+    
+}
