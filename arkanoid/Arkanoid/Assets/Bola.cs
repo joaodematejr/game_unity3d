5b6ffa1224c538bf237718989656d511e3c51c6f
joaodematejr <joaodematejr@gmail.com>
2019-06-10 22:21:31 -0300

    Adicionado Script Bolinha

---
+++ arkanoid/Arkanoid/Assets/Bola.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour {
+
+    public Vector2 Velocidade;
+
+    // Start is called before the first frame update
+    void Start () {
+
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D> ();
+        rigidbody.AddForce (Velocidade);
+
+    }
+
+}
